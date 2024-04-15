608b2594b3a3f02709ab8eb7efe2708cc949ba0d
Ali Mousavi <amousavig@icloud.com>
2024-04-16 00:43:39 +0330

    Successfully initialized push user.

--- src/app/(authenticated)/newchat/page.tsx
+++ src/app/(authenticated)/newchat/page.tsx
@@ -1,48 +1,94 @@
 "use client";
 
-import { sendInvitationEmail } from "@/app/lib/actions";
-import { fetchProfile } from "@/app/lib/data";
-import { User, UserProfile } from "@/app/lib/definitions";
+import { requestNewChat, sendInvitationEmail } from "@/app/lib/actions";
+import { InitSessionInfo } from "@/app/lib/definitions";
 import { AuthContext } from "@/app/ui/layout/AuthProvider";
 import { ProfileContext } from "@/app/ui/layout/ProfileProvider";
+import { PushContext } from "@/app/ui/layout/PushProvider";
 import { useContext, useEffect, useState } from "react";
 
 export default function page() {
   // const auth = useContext(AuthContext);
+  const push = useContext(PushContext);
   const userProfile = useContext(ProfileContext);
-  const [userInfo, setUserInfo] = useState<User | null>(null);
-  // const [profile, setProfile] = useState<UserProfile | null>(null);
+  const [sessionInfo, setSessionInfo] = useState<InitSessionInfo>({
+    title: "",
+    description: "",
+    email: "",
+  });
 
-  // useEffect(() => {
-  //   auth?.getUserInfo()?.then((userInfo) => {
-  //     setUserInfo({
-  //       uuid: userInfo?.uuid!,
-  //       address: userInfo?.wallets[0].public_address!,
-  //     });
-  //   });
-  // }, [auth?.isAuthenticated]);
-
-  // useEffect(() => {
-  //   const getProfile = async () => {
-  //     const profile = await fetchProfile(userInfo?.uuid!);
-  //     setProfile(profile);
-  //   };
-  //   getProfile();
-  // }, [userInfo]);
-
-  const sendTestEmail = async () => {
-    const response = await sendInvitationEmail(
-      "amousavig@icloud.com",
+  const requestChat = async (e: React.FormEvent) => {
+    e.preventDefault();
+    // const pushApi: any = push?.pushUser;
+    const createdGroup = await push?.pushUser?.chat.group.create(
+      sessionInfo.title,
+      {
+        description: sessionInfo.description,
+      }
+    );
+    console.log("💀", createdGroup);
+    const response = await requestNewChat(
       userProfile!,
-      "asdf1234"
+      sessionInfo,
+      createdGroup?.chatId!
     );
 
-    console.log(response);
+    console.log("⚽️", response);
+  };
+
+  const initUser = async () => {
+    try {
+      // console.log("👉🏻", auth?.user?.address);
+      // console.log("👉🏻", auth?.ethers?.getSigner(auth.user?.address!));
+      // console.log("👉🏻", auth?.ethersSigner());
+      // await push?.pushInit(auth?.ethers?.getSigner(auth.user?.address!)!);
+      const pushUser = await push?.pushInit();
+    } catch (error) {
+      console.error("🌈", error);
+    }
   };
   return (
     <div>
-      new chat <button onClick={sendTestEmail}>SEND TEST EMAIL</button>
+      new chat
+      <div>
+        <button onClick={initUser}>INITIALIZE PUSH USER</button>
+      </div>
+      <form
+        className="m-3 flex flex-col items-start gap-3 border p-3"
+        onSubmit={requestChat}
+      >
+        <input
+          type="email"
+          placeholder="email"
+          value={sessionInfo.email}
+          onChange={(e) =>
+            setSessionInfo({ ...sessionInfo, email: e.target.value })
+          }
+          className="border"
+        />
+        <input
+          type="text"
+          placeholder="title"
+          value={sessionInfo.title}
+          onChange={(e) =>
+            setSessionInfo({ ...sessionInfo, title: e.target.value })
+          }
+          className="border"
+        />
+        <textarea
+          placeholder="description"
+          value={sessionInfo.description}
+          onChange={(e) =>
+            setSessionInfo({ ...sessionInfo, description: e.target.value })
+          }
+          className="border"
+        />
+        <button type="submit" className="bg-blue-200">
+          submit
+        </button>
+      </form>
       <div>{JSON.stringify(userProfile)}</div>
+      <div>{JSON.stringify(push?.pushUser)}</div>
     </div>
   );
 }
