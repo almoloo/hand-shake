11bcde72146ea9940a52348ee3c134e8c086b3a1
Ali Mousavi <amousavig@icloud.com>
2024-04-17 02:31:01 +0330

    Made push protocol work with metamask signer.

--- src/app/ui/layout/PushProvider.tsx
+++ src/app/ui/layout/PushProvider.tsx
@@ -1,12 +1,10 @@
 "use client";
 
 import type { PushAPI as PushAPIType } from "@pushprotocol/restapi";
-import { CONSTANTS, PushAPI, SignerType } from "@pushprotocol/restapi";
-import React, { createContext, useContext, useEffect, useState } from "react";
-import { AuthContext } from "@/app/ui/layout/AuthProvider";
+import { CONSTANTS, PushAPI } from "@pushprotocol/restapi";
+import React, { createContext, useState } from "react";
 import { PushContextType } from "@/app/lib/definitions";
 import { ethers } from "ethers";
-import { useSDK } from "@metamask/sdk-react";
 
 export const PushContext = createContext<PushContextType | null>(null);
 
@@ -15,10 +13,6 @@ export default function PushProvider({
 }: {
   children: React.ReactNode;
 }) {
-  const { sdk, provider, ready, account } = useSDK();
-  const auth = useContext(AuthContext);
-  const [signer, setSigner] = useState(null);
-
   const initPushUser = async () => {
     const signer = await new ethers.BrowserProvider(
       window.ethereum!
@@ -26,11 +20,9 @@ export default function PushProvider({
     const env = process.env.NEXT_PUBLIC_TESTNET
       ? CONSTANTS.ENV.STAGING
       : CONSTANTS.ENV.PROD;
-    console.log("✨ signer", signer);
     const pushUser = await PushAPI.initialize(signer, {
       env,
     });
-    console.log("🪁", pushUser);
     setPushInfo({ ...pushInfo, pushUser });
     return pushUser;
   };
