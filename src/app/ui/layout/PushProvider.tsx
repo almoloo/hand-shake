3b2c06bad02c710dfbe90808b03e8d8c2dd739a6
Ali Mousavi <amousavig@icloud.com>
2024-04-19 22:31:31 +0330

    Removed junk.

--- src/app/ui/layout/PushProvider.tsx
+++ src/app/ui/layout/PushProvider.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-// import type { PushAPI as PushAPIType } from "@pushprotocol/restapi";
 import { CONSTANTS, PushAPI } from "@pushprotocol/restapi";
 import React, { createContext, useState } from "react";
 import { PushContextType } from "@/app/lib/definitions";
 import { ethers } from "ethers";
+import { toast } from "sonner";
 
 export const PushContext = createContext<PushContextType | null>(null);
 
@@ -14,22 +14,25 @@ export default function PushProvider({
   children: React.ReactNode;
 }) {
   const initPushUser = async () => {
-    // const signer = await new ethers.BrowserProvider(
-    //   window.ethereum!
-    // ).getSigner();
-    const signer = await new ethers.providers.Web3Provider(
-      window.ethereum as any,
-      "any"
-    ).getSigner();
-    const env = process.env.NEXT_PUBLIC_TESTNET
-      ? CONSTANTS.ENV.STAGING
-      : CONSTANTS.ENV.PROD;
-    const pushUser = await PushAPI.initialize(signer, {
-      env,
-    });
-    console.log("🚀 pushUser", pushUser);
-    setPushInfo({ ...pushInfo, pushUser });
-    return pushUser;
+    try {
+      const signer = await new ethers.providers.Web3Provider(
+        window.ethereum as any,
+        "any"
+      ).getSigner();
+      const env = process.env.NEXT_PUBLIC_TESTNET
+        ? CONSTANTS.ENV.STAGING
+        : CONSTANTS.ENV.PROD;
+      const pushUser = await PushAPI.initialize(signer, {
+        env,
+      });
+      setPushInfo({ ...pushInfo, pushUser });
+      toast.success("Push user initialized");
+      return pushUser;
+    } catch (error) {
+      toast.error("Failed to initialize push user");
+      console.error("🌈", error);
+      return null;
+    }
   };
   const [pushInfo, setPushInfo] = useState<PushContextType>({
     pushUser: null,
